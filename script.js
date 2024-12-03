51ab697cbe9cdcf1193d3c6db3ce3af2a14c0502
pritimanbala <85382528+pritimanbala@users.noreply.github.com>
2024-12-03 23:56:08 +0530

    the final upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showBar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideBar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
